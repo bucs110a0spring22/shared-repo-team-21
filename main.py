bf4206fbeb5f6b7f8abea23f37554dec46fc1be7
Aiden Lin <98539623+AidenL173@users.noreply.github.com>
2022-02-08 20:02:58 +0000

    Started Exercise.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+integer1 = int(input("Enter Integer "))
+integer2 = int(input("Enter Another Integer "))
+integer3 = int(input("Enter 3rd Integer "))
+integer4 = int(input("Enter 4th Integer "))
+mylist = (integer1, integer2, integer3, integer4)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
